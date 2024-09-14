6e00f5894f2d69a3708efdf45b3aad90302d76fb
lapollinaria <linasaintp@icloud.com>
2024-09-15 00:57:11 +0300

    Added table and a form for suggestions

--- .venv/app.py
+++ .venv/app.py
@@ -1,5 +1,6 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
 import os.path
 
 db_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, "database.db")
@@ -10,8 +11,17 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-app = Flask(__name__)
+def get_choreos(n_of_m):
+    conn = get_db_connection()
+    chor = conn.execute('SELECT * FROM gmedium WHERE members=?',
+                        (n_of_m,)).fetchall()
+    conn.close()
+    if chor is None:
+        abort(404)
+    return chor
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '987654321'
 
 @app.route('/')
 def index():
@@ -19,3 +29,26 @@ def index():
     choreos = conn.execute('SELECT * FROM gmedium').fetchall()
     conn.close()
     return render_template('index.html', choreos=choreos)
+
+@app.route('/<int:n_of_m>')
+def chor(n_of_m):
+    chor = get_choreos(n_of_m)
+    return render_template('options.html', chor=chor)
+
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        members = request.form['members']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO suggestions (title, members) VALUES (?, ?)',
+                         (title, members))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    return render_template('create.html')
